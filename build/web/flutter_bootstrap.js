748dba3c5d46d8a3d9b986bbaa0e38795ac3ac2f
promaha <87933321+promaha@users.noreply.github.com>
2025-02-12 09:11:30 +0300

    edit 3

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "671719936"
+    serviceWorkerVersion: "1929673897"
   }
 });
